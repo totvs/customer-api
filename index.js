f5ac1dbf1a3c3029aad50d3c590010a765931185
Rafael Quines <rafael.quines@totvs.com.br>
2017-05-08 14:19:19 -0300

    enabled cors

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ function initRestServer() {
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.use(function (req, res, next) {
         logger.info('Rest ' + req.method + ' Request on ' + req.originalUrl);
         next();
     });
-
     app.use('/customer', customerRouter);
-
     app.listen(config.restserver.port, function () {
         logger.info('Rest Server started on port ' + config.restserver.port);
     });
