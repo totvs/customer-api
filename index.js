ce83140226832d06d84f83e4bb5024f3625f609f
Rafael Quines <rafael.quines@totvs.com.br>
2017-11-24 18:38:48 -0200

    dev

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function initRestServer() {
     }));
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE, PATCH');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
